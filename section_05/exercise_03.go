f10504bc5083e23264e2f02491c14c6386d34469
Scott E. Russell <3295553+AmundsenJunior@users.noreply.github.com>
2019-05-23 17:44:27 -0400

    Create exercise_03.go

---
+++ section_05/exercise_03.go
@@ -0,0 +1,14 @@
+/*
+  Using the code from the previous exercise,
+  At the package level scope, assign the following values to the three variables
+  for x assign 42
+  for y assign “James Bond”
+  for z assign true
+  in func main
+  use fmt.Sprintf to print all of the VALUES to one single string. 
+  ASSIGN the returned value of TYPE string using the short declaration operator to a VARIABLE with the IDENTIFIER “s”
+  print out the value stored by variable “s”
+  code: here’s the solution: https://play.golang.org/p/QFctSQB_h3 
+  */
+  
+  
