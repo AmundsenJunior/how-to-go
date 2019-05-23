87f6c44f4a58ef1aa75f733f2457f12c88e4cc8a
Scott E. Russell <3295553+AmundsenJunior@users.noreply.github.com>
2019-05-23 17:36:33 -0400

    Update exercise_01.go

--- section_05/exercise_01.go
+++ section_05/exercise_01.go
@@ -1,11 +1,13 @@
-// Using the short declaration operator, ASSIGN these VALUES to VARIABLES with the IDENTIFIERS “x” and “y” and “z”
-// 42
-// “James Bond”
-// true
-// Now print the values stored in those variables using
-// a single print statement
-// multiple print statements
-// code: here’s the solution: https://play.golang.org/p/yYXnWXIQNa
+/*
+  Using the short declaration operator, ASSIGN these VALUES to VARIABLES with the IDENTIFIERS “x” and “y” and “z”
+  42
+  “James Bond”
+  true
+  Now print the values stored in those variables using
+  a single print statement
+  multiple print statements
+  code: here’s the solution: https://play.golang.org/p/yYXnWXIQNa
+*/
 
 package main
 
